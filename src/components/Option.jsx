d32eaa9d23a8caa42193fa92c2639e546033acd3
joaogabrielmc <joaog.martinsc@gmail.com>
2023-10-30 13:12:32 -0300

    30.10

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -20,7 +20,7 @@ export function Option({ index, answer, handleOptionClick, isSelected, src, opti
       className={`${cardClassName} flex flex-col px-8 py-[46px] bg-gray-5  rounded-lg w-[250px] border-x-[3.194px] border-t-[3.194px] border-b-[6.387px] ease-in-out duration-300 cursor-pointer hover:bg-gray-4`}
     >
       <img className="self-end translate-x-[19px] -translate-y-[33px]" src={`./src/assets/${svg}.svg`} alt="" />
-      <img className="w-full" src={`./src/assets/${src}.svg`} alt="" />
+      <img className="w-full" src={`./${src}.svg`} alt="" />
       <span className="text-center text-base font-normal leading-[17.6px] mt-6">{optionText}</span>
     </li>
   )

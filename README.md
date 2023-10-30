6209cc0c7665335c18d7f9a989b53e49b237d329
joaogabrielmc <joaog.martinsc@gmail.com>
2023-10-30 11:11:59 -0300

    commit unico

---
+++ README.md
@@ -0,0 +1,17 @@
+<strong><h1 align="center">Bora Codar #38 - Quiz Time</h1></strong>
+
+<p align="center">
+  <img alt="" src=".github/preview.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- ReactJS
+- Tailwind
+
+
+---
+
+Feito com ♥ by [Rocketseat](https://www.rocketseat.com.br/)
---
+++ src/data.js
@@ -0,0 +1,97 @@
+export const questions = [
+  {
+    id: 1,
+    question: "1. Qual piloto conquistou o maior número de Campeonatos Mundiais na Fórmula 1?",
+    options: [
+      {
+        id: "image-1",
+        text: "Ayrton Senna",
+      },
+      {
+        id: "image-2",
+        text: "Lewis Hamilton",
+      },
+      {
+        id: "image-3",
+        text: "Michael Schumacher",
+      },
+    ],
+    correctAnswer: "Lewis Hamilton"
+  },
+  {
+    id: 2,
+    question: "2. Quem detém o recorde de mais vitórias em corridas na Fórmula 1?",
+    options: [
+      {
+        id: "image-1",
+        text: "Ayrton Senna",
+      },
+      {
+        id: "image-2",
+        text: "Lewis Hamilton",
+      },
+      {
+        id: "image-3",
+        text: "Michael Schumacher",
+      },
+    ],
+    correctAnswer: "Lewis Hamilton"
+  },
+  {
+    id: 3,
+    question: '3. Qual piloto é conhecido por suas habilidades em condições de chuva e é frequentemente referido como o "mágico da chuva"?',
+    options: [
+      {
+        id: "image-1",
+        text: "Ayrton Senna",
+      },
+      {
+        id: "image-2",
+        text: "Lewis Hamilton",
+      },
+      {
+        id: "image-3",
+        text: "Michael Schumacher",
+      },
+    ],
+    correctAnswer: "Ayrton Senna"
+  },
+  {
+    id: 4,
+    question: "4. Quem é famoso por sua longa parceria com a equipe Ferrari e pela conquista de muitos títulos com a equipe italiana?",
+    options: [
+      {
+        id: "image-1",
+        text: "Ayrton Senna",
+      },
+      {
+        id: "image-2",
+        text: "Lewis Hamilton",
+      },
+      {
+        id: "image-3",
+        text: "Michael Schumacher",
+      },
+    ],
+    correctAnswer: "Michael Schumacher"
+  },
+  {
+    id: 5,
+    question: "5. Qual piloto é amplamente reconhecido por seu carisma e pelo impacto que teve na popularização da Fórmula 1?",
+    options: [
+      {
+        id: "image-1",
+        text: "Ayrton Senna",
+      },
+      {
+        id: "image-2",
+        text: "Lewis Hamilton",
+      },
+      {
+        id: "image-3",
+        text: "Michael Schumacher",
+      },
+    ],
+    correctAnswer: "Ayrton Senna"
+  }
+]
